d63affbbec260e13ec749e7074e8bf5dc979549e
miguelalbryan <43114309+miguelalbryan@users.noreply.github.com>
2019-10-10 11:07:29 -0400

    Updates

---
+++ precache-manifest.dffef44c054c5fa47d42194f2c1e1e73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3484d677de75a292f36868d76400cbe0",
+    "url": "/junotechchallenge-miguel-albryan-bonus/index.html"
+  },
+  {
+    "revision": "b21598b433ca8e36ccc6",
+    "url": "/junotechchallenge-miguel-albryan-bonus/static/css/main.517b9700.chunk.css"
+  },
+  {
+    "revision": "702c3dae5ea852b86479",
+    "url": "/junotechchallenge-miguel-albryan-bonus/static/js/2.15cdc988.chunk.js"
+  },
+  {
+    "revision": "b21598b433ca8e36ccc6",
+    "url": "/junotechchallenge-miguel-albryan-bonus/static/js/main.f39e201c.chunk.js"
+  },
+  {
+    "revision": "27849ae0642a47c53131",
+    "url": "/junotechchallenge-miguel-albryan-bonus/static/js/runtime-main.adde6777.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/junotechchallenge-miguel-albryan-bonus/precache-manifest.dffef44c054c5fa47d42194f2c1e1e73.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/junotechchallenge-miguel-albryan-bonus/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
