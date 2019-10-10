fa8e9cdd4c365e4bda16e21e649af517a7dc065b
miguelalbryan <43114309+miguelalbryan@users.noreply.github.com>
2019-10-10 04:52:16 -0400

    initial commit

---
+++ src/components/Actors.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+
+import MovieDetails from './MovieDetails.js';
+class Actors extends Component{
+   constructor(props){
+      super(props);
+
+   }
+    render(){
+       return(
+         <div> <MovieCard /> </div>
+       )
+      }
+
+}
+
+export default Movies
